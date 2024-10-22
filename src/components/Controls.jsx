0361246cad0095156f85e6168f29dbf2b22e4853
Andrei Stroianetki <sandreasweb@gmail.com>
2024-10-22 23:27:20 +0200

    search completed

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 import Search from "./Search";
 import CustomSelect from "./CustomSelect";
@@ -23,9 +23,17 @@ const Wrapper = styled.div`
   }
 `;
 
-const Controls = () => {
+const Controls = ({onSearch}) => {
   const [search, setSearch] = useState("");
   const [region, setRegion] = useState("");
+
+  useEffect(() => {
+    const regionValue = region?.value || "";
+
+
+    onSearch(search, regionValue);
+    // eslint-disable-next-line
+  } , [search, region]);
 
   return (
     <Wrapper>
@@ -41,4 +49,6 @@ const Controls = () => {
     </Wrapper>
   );
 };
+
+
 export default Controls;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import { IoMoon, IoMoonOutline } from "react-icons/io5";
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 import { Container } from "./Container";
 
@@ -16,7 +17,7 @@ const Wrapper = styled.div`
   padding: 2rem 0;
 `;
 
-const Title = styled.a.attrs({
+const Title = styled(Link).attrs({
   to: "/",
 })`
   color: var(--colors-text);
