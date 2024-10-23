6f2325c48bcaf47ad3f406acc2bb8369607eb1c8
Andrei Stroianetki <sandreasweb@gmail.com>
2024-10-23 15:37:04 +0200

    adding detailspage content

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,10 +1,26 @@
-import { useParams } from 'react-router-dom';
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { IoArrowBack } from "react-icons/io5";
+import { searchByCountry } from "../config";
 
 const Details = () => {
-  const { name } = useParams();  
+  const { name } = useParams();
+  const { navigate, goBack} = useNavigate();
+  const [country, setCountry] = useState(null);
+
+  useEffect(() => {
+    axios.get(searchByCountry(name)).then(({ data }) => setCountry(data[0]));
+  }, [name]);
 
   return (
-    <div>Details {name}</div>
+    <div>
+      <button onClick={goBack}>
+        <IoArrowBack size="20px" /> Back
+      </button>
+      Details {name}
+    </div>
   );
 };
 
